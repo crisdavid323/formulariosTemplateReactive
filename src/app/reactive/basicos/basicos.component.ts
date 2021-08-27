85de3631229279d75d72eb5e3dd32681af8eac5e
Cristhian David <crisdavid323@gmail.com>
2021-08-26 23:05:06 -0500

    Curso terminado de Formularios Reactive

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basicos.component.css']
 })
 export class BasicosComponent implements OnInit {
+  // FormControl
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 8040ti'),
+  //   precio: new FormControl(1500),
+  //   exsitencias: new FormControl(5),
+  // })
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.minLength(1)]],
+    existencias: [, [Validators.required, Validators.minLength(1)]],
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTX',
+      existencias: 1550
+    })
   }
 
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors
+      && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset()
+  }
 }
